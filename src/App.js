bd3ad847f9956d0fd18d2b75bf18646192174c1a
Michele Lorusso <miky-l@hotmail.it>
2023-02-24 15:27:19 +0100

    add other page, project view Udemy React [2023]

--- src/App.js
+++ src/App.js
@@ -1,32 +1,36 @@
-import Accordion from "./components/Accordion";
-import ButtonPage from "./pages/ButtonPage";
+import Sidebar from './components/Sidebar';
+import Route from './components/Route';
+import AccordionPage from './pages/AccordionPage';
+import DropdownPage from './pages/DropdownPage';
+import ButtonPage from './pages/ButtonPage';
+import ModalPage from './pages/ModalPage';
+import TablePage from './pages/TablePage';
+import CounterPage from './pages/CounterPage';
 
 function App() {
-  const items = [
-    {
-      id: "l2kj5",
-      label: "Can I use React on a project?",
-      content:
-        "You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.",
-    },
-    {
-      id: "lk2j35lkj",
-      label: "Can I use Javascript on a project?",
-      content:
-        "You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.",
-    },
-    {
-      id: "l1kj2i0g",
-      label: "Can I use CSS on a project?",
-      content:
-        "You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.",
-    },
-  ];
-
   return (
-    <div>
-      <Accordion items={items} />
-      <ButtonPage></ButtonPage>
+    <div className="container mx-auto grid grid-cols-6 gap-4 mt-4">
+      <Sidebar />
+      <div className="col-span-5">
+        <Route path="/accordion">
+          <AccordionPage />
+        </Route>
+        <Route path="/">
+          <DropdownPage />
+        </Route>
+        <Route path="/buttons">
+          <ButtonPage />
+        </Route>
+        <Route path="/modal">
+          <ModalPage />
+        </Route>
+        <Route path="/table">
+          <TablePage />
+        </Route>
+        <Route path="/counter">
+          <CounterPage initialCount={10} />
+        </Route>
+      </div>
     </div>
   );
 }
